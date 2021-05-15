28d840b4a3a626cbad04428648b2abea022aff92
LuisFlahan4051 <luisflahan4051@gmail.com>
2021-05-15 06:49:36 -0500

    Falta crear la busqueda binaria y termino.

--- main.c
+++ main.c
@@ -7,6 +7,15 @@
 #include <math.h>
 #include <string.h>
 #include <ctype.h> // <-- Manejo tipoc. Conversion de un caracter a mayusculas o minusculas.
+
+// Biblioteca dependiente del sistema operativo. <-- sleep(1 segundo);
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <unistd.h>
+#endif
+
+#define MAXCHAR 20
 #define LC LC_ALL
 #define ANSI_COLOR_RED "\x1b[31m"
 #define ANSI_COLOR_GREEN "\x1b[32m"
@@ -16,10 +25,9 @@
 #define ANSI_COLOR_CYAN "\x1b[36m"
 #define ANSI_COLOR_RESET "\x1b[0m"
 
-void llenar_datos_aleatorios(int arreglo[], int tamanio, int limiteAleatorioInferior, int limiteAleatorioSuperior)
-{
-    for (int i = 0; i < tamanio; i++)
-    {
+
+void llenar_datos_aleatorios(int arreglo[], int tamanio, int limiteAleatorioInferior, int limiteAleatorioSuperior){
+    for (int i = 0; i < tamanio; i++){
         arreglo[i] = rand() % (limiteAleatorioSuperior + 1 - limiteAleatorioInferior);
     }
 }
@@ -118,7 +126,7 @@ int invertir_cadena(char string[], int tamanio, int posicionInicial){
 }
 
 void programa3(){
-    char string[20];
+    char string[MAXCHAR];
     printf(ANSI_COLOR_YELLOW"\nIngrese una cadena, no más de 20 caracteres:\n> "ANSI_COLOR_RESET);
     scanf("%s",&string);
     
@@ -141,14 +149,14 @@ int convertir_cadena_a_minusculas(char string[], int tamanio, int posicionInicia
 
 void programa4(){
     printf(ANSI_COLOR_YELLOW"\nIngrese una cadena, no más de 20 caracteres:\n> "ANSI_COLOR_RESET);
-    char string[20];
+    char string[MAXCHAR];
     scanf("%s",&string);
 
     convertir_cadena_a_minusculas(string, strlen(string), 0);
     printf(ANSI_COLOR_MAGENTA"\nCadena areglada:"ANSI_COLOR_RESET);
     printf("\n%s", string);
 
-    char stringInvertida[20];
+    char stringInvertida[MAXCHAR];
     strcpy(stringInvertida, string);
     
     invertir_cadena(stringInvertida,strlen(stringInvertida),0);
@@ -164,40 +172,129 @@ void programa4(){
     }
 }
 
+void quickSort(int arreglo[], int izq, int der){
+    int i = 0, j = 0, v = 0, aux = 0, bandera = 1;
+	i = izq;
+	j = der;
+    v = arreglo[(izq + der) / 2];
+
+    while (bandera != 0){
+        while ((arreglo[i] < v) && (j <= der)){
+            i++;
+        }
+        while((v < arreglo[j]) && (j > izq)){
+            j--;
+        }
+        if (i <= j){
+            aux = arreglo[i];
+            arreglo[i] = arreglo[j]; 
+            arreglo[j] = aux;
+            i++; 
+            j--;
+        }
+        if(!(i <= j)){
+            bandera = 0;
+        }
+    }
+    if (izq < j){
+        quickSort(arreglo, izq, j);
+    }
+    if (izq < j){
+        quickSort(arreglo, i, der);
+    }
+}
+
+int busqueda_lineal(int arreglo[], int tamanio, int buscarNumero, int posicionInicial){
+    if(arreglo[posicionInicial]==buscarNumero)
+    return posicionInicial;
+
+    if (posicionInicial >= tamanio - 1)
+    return 0;
+    
+    busqueda_lineal(arreglo, tamanio, buscarNumero, ++posicionInicial);
+}
+
+void programa5(){
+    int tamanio = 10;
+    int arreglo[10];
+    llenar_datos_aleatorios(arreglo, tamanio, 0, 10);
+
+    printf(ANSI_COLOR_YELLOW "\nArreglo original:\n" ANSI_COLOR_RESET);
+    impresion_arreglo_recursivo(arreglo, tamanio, 0);
+    printf("\n");
+
+    printf(ANSI_COLOR_YELLOW"\nNúmero a buscar:\n> "ANSI_COLOR_RESET);
+    int buscarNumero = 0;
+    scanf("%d",&buscarNumero);
+
+    printf(ANSI_COLOR_MAGENTA"\nEl numero fue encontrado en la posición:"ANSI_COLOR_RESET); 
+    printf(" %d\n", busqueda_lineal(arreglo,tamanio,buscarNumero,0));
+
+    printf(ANSI_COLOR_MAGENTA "\nArreglo ordenado:\n" ANSI_COLOR_RESET);
+    quickSort(arreglo,0,tamanio-1);
+    impresion_arreglo_recursivo(arreglo, tamanio, 0);
+    printf("\n");
+
+    printf(ANSI_COLOR_MAGENTA "\nEl numero fue encontrado en la posición:" ANSI_COLOR_RESET);
+    printf(" %d\n", busqueda_lineal(arreglo, tamanio, buscarNumero, 0));
+    
+}
+
+void programa6(){
+
+}
+
 int main(){
     setlocale(LC, ""); // Hibilita los carácteres de la zona.
     srand(time(NULL)); // Fija una semilla para utilizar rand.
-
-    printf("\n1)Imprimir arreglo.\n2)Invetir arreglo.\n3)Invertir cadena.\n4)Palíndromos.");
-    printf("\nIngresa la opción a probar:\n> ");
-    int opcion;
-    scanf("%d",&opcion);
-
-    switch (opcion)
+    int bandera = 1;
+    while ( bandera != 0)
     {
-    case 1:
-        programa1();
-        break;
-    case 2:
-        //printf("%d\n", 7/2); --> 3
-        programa2();
-        break;
-    case 3:
-        programa3();
-        break;
-    case 4:
-        programa4();
-        /*
-        printf(ANSI_COLOR_RED "This text is RED!" ANSI_COLOR_RESET "\n");
-        printf(ANSI_COLOR_GREEN "This text is GREEN!" ANSI_COLOR_RESET "\n");
-        printf(ANSI_COLOR_YELLOW "This text is YELLOW!" ANSI_COLOR_RESET "\n");
-        printf(ANSI_COLOR_BLUE "This text is BLUE!" ANSI_COLOR_RESET "\n");
-        printf(ANSI_COLOR_MAGENTA "This text is MAGENTA!" ANSI_COLOR_RESET "\n");
-        printf(ANSI_COLOR_CYAN "This text is CYAN!" ANSI_COLOR_RESET "\n");*/
-        break;
-    default:
-        break;
+        printf(ANSI_COLOR_GREEN"\n1)Imprimir arreglo.\t2)Invetir arreglo.");
+        printf("\n3)Invertir cadena. \t4)Palíndromos.");
+        printf("\n5)Búsqueda Linal.  \t6)Busqueda Binaria.\n");
+        printf("\nIngresa la opción a probar:\n> "ANSI_COLOR_RESET);
+        int opcion;
+        scanf("%d", &opcion);
+
+        switch (opcion)
+        {
+        case 1:
+            programa1();
+            break;
+        case 2:
+            //printf("%d\n", 7/2); --> 3
+            programa2();
+            break;
+        case 3:
+            programa3();
+            break;
+        case 4:
+            programa4();
+            break;
+        case 5:
+            programa5();
+            break;
+        case 6:
+            programa6();
+            break;
+        default:
+            bandera = 0;
+            break;
+        }
+
+        if (opcion<= 0 || opcion >= 7){    
+            printf(ANSI_COLOR_CYAN"\n\nTen un buen dia! UwU"ANSI_COLOR_RESET);
+            break;
+        }else{
+            printf(ANSI_COLOR_GREEN"\n¿Probar otro programa?"ANSI_COLOR_RESET);
+        }
     }
+    
+    
+
+    printf(ANSI_COLOR_CYAN "\n\n< By LuisFlahan4051  ;) />\n\n" ANSI_COLOR_RESET);
+    sleep(2);
 
     return EXIT_SUCCESS;
 }
